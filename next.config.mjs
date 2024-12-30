47ad405a77efdc79f28a11c5105ad29f4a51b83c
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-12-30 16:33:21 +0100

    simplify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/lorefais', // Subpath for GitHub Pages
-  assetPrefix: '/lorefais', // Prefix for static assets
-  output: 'export', // Enables static export
 };
 
 export default nextConfig;
