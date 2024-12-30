4e46459855e3ae0a141f852ec49d1bdf20f4fd00
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-12-30 14:45:19 +0100

    deploying v0.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/lorefais', 
+  assetPrefix: '/lorefais', 
+  output: 'export',  
 };
 
 export default nextConfig;
